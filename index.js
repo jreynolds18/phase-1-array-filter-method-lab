8e5302ae3760169374df1d32948cee5ff6991fd0
Jesse Reynolds <jessereynolds18@gmail.com>
2023-08-28 21:31:22 -0500

    create three functions to find matching drivers, find matching drivers by first letter, and find all info about a named driver

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+    ];
+
+
+function findMatching(array, namedPerson) {
+  let newListOfDrivers = [];
+  for (let member of array) {
+    if(member.toUpperCase() === namedPerson.toUpperCase()) {
+      newListOfDrivers.push(member);
+    }
+  }
+  return newListOfDrivers;
+}
+
+//Function to query the array to filter out a new array with all namedPersons starting with the same letter
+function fuzzyMatch(array, namedPerson) {
+    let newListOfDrivers = [];
+    for (let member of array) {
+      if(member.charAt(0) === namedPerson.charAt(0)) {
+        newListOfDrivers.push(member);
+      }
+    }
+    return newListOfDrivers;
+}
+
+//Function to take the array and return each element of the provided namedPerson
+function matchName(array, namedPerson) {
+    let newListOfDrivers = [];
+    for (let infoBlock of array) {
+      if(infoBlock.name === namedPerson) {
+        newListOfDrivers.push(infoBlock);
+      }
+    }
+    return newListOfDrivers;
+}
